e79a336d6593f9b0a2aeeeb4db61b7981a21ec47
Amanda <amandabaydar@gmail.com>
2022-01-11 14:38:06 -0500

    imports to auth route and set up get route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 
 
 // @route   GET api/auth
-// @desc    test route
+// @desc    Get user by token
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'))
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch(err){
+        res.status(500).send('Server error')
+    }
+})
 
 module.exports = router
